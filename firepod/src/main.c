0fd28c51b8efa1a50c8381f945e5596a6c4a438b
Charlie Amante <amante.charlie@gmail.com>
2025-09-19 20:33:06 -0400

    Blinky example builds for nucleo_l476rg dev board

--- firepod/src/main.c
+++ firepod/src/main.c
@@ -4,16 +4,16 @@
 #include <zephyr/drivers/gpio.h>
 
 // This is accessing the DeviceTree node through its label (can see the entire devicetree in generated zephyr.dts file in the build directory)
-#define LED0 DT_NODELABEL(led0)
+#define LED2 DT_NODELABEL(green_led_2)
 
 // 'gpios' refers to the property name within the led0 node, which is itself inside the leds parent node in the devicetree
 // you can see this^^ in code if you search for the 'zephyr/boards/nordic/nrf54l15dk/nrf54l15dk_common.dtsi' file
-static const struct gpio_dt_spec led_0 = GPIO_DT_SPEC_GET(LED0, gpios);
+static const struct gpio_dt_spec led_2 = GPIO_DT_SPEC_GET(LED2, gpios);
 
 int main(void)
 {
     // led_0.port is used because you need to check if the gpio controller itself is ready to be used, if you just do led_0.pin you will get a number like '9'
-    if (!device_is_ready(led_0.port))
+    if (!device_is_ready(led_2.port))
     {
         return 0;
     }
@@ -22,7 +22,7 @@ int main(void)
 
     int ret;
     // configuration of led_0 pin
-    ret = gpio_pin_configure_dt(&led_0, GPIO_OUTPUT);
+    ret = gpio_pin_configure_dt(&led_2, GPIO_OUTPUT);
 
     if (ret != 0)
     {
@@ -32,7 +32,7 @@ int main(void)
     while (true)
     {
         state = !state;
-        ret = gpio_pin_set_dt(&led_0, state);
+        ret = gpio_pin_set_dt(&led_2, state);
         if (ret != 0)
         {
             return 0;
