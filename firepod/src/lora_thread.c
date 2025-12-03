62f8eeb2b043f0068370b5836148a028c36912a6
Charlie Amante <amante.charlie@gmail.com>
2025-12-02 19:50:45 -0500

    Added most of the custom devicetree and made a few changes to the lora code

--- firepod/src/lora_thread.c
+++ firepod/src/lora_thread.c
@@ -7,8 +7,28 @@ static const struct spi_dt_spec sx1262 = SPI_DT_SPEC_GET(SX1262, SPI_WORD_SET(8)
 
 void lora_thread(void *a1, void *a2, void *a3)
 {
+    // first configure the lora device using something like this
+    /*
+    struct lora_modem_config config = {
+    .frequency = 915000000,
+    .bandwidth = BW_125_KHZ,
+    .datarate = SF_7,
+    .coding_rate = CR_4_5,
+    .preamble_len = 8,
+    .tx_power = 14,
+    .tx = false,   // this field is ignored now
+    };
+
+    lora_config(lora_dev, &config);
+    */
+
     while (true)
     {
+        /*
+        1. format the data taken from message queue that gets posted to by the i2c thread into a payload
+        2. use lora_send to send payload (DIO 1 is handled under the hood by hte sx1262 drivers)
+        */
+
         bool ret = check_heartbeat();
 
         printk("heartbeat: %d\n", (int)ret);
