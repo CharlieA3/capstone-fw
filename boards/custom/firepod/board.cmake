dc659d986afea6d910747009d7337103c6ec8cd4
Charlie Amante <amante.charlie@gmail.com>
2025-12-03 15:03:02 -0500

    This flashes to the custom PCB

--- boards/custom/firepod/board.cmake
+++ boards/custom/firepod/board.cmake
@@ -1,1 +1,2 @@
-include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
+board_runner_args(openocd "--config=interface/stlink.cfg" "--config=target/stm32l4x.cfg")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
